d3ff2491a73d81d6961f96c36ff664f440c1a72c
David <85349822+Coderx-Gamer@users.noreply.github.com>
2023-11-21 15:31:11 +0000

    Fix TLD typo in ModInfo.

--- src/main/java/org/cmdutils/ModInfo.java
+++ src/main/java/org/cmdutils/ModInfo.java
@@ -3,7 +3,7 @@ package org.cmdutils;
 public class ModInfo {
     public static final String VERSION = "1.0.0";
     public static final String LICENSE = "GNU General Public License 3";
-    public static final String WEBSITE = "https://cmd-utils.com/ (coming soon)";
+    public static final String WEBSITE = "https://cmd-utils.org/ (coming soon)";
     public static final String SOURCE = "https://github.com/Coderx-Gamer/cmd-utils/";
     public static final String[] AUTHORS = {
             "Coderx_Gamer"
